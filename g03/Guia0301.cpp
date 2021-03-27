97d215126ee3afa888315931351e91f6122cdb28
Pedro Amorim Sa <pedrohasa@gmail.com>
2021-03-27 11:04:56 -0300

    iniciar g03, incluir v0.1

---
+++ g03/Guia0301.cpp
@@ -0,0 +1,169 @@
+/**
+ * Guia0301.cpp - v0.1 - 18/3/2021
+ * Author: Pedro H. Amorim Sa - 742626
+ * 
+ * Para compilar em um terminal:
+ * 
+ * No Linux  : g++ -o Guia0301 ./Guia0301.cpp
+ * No Windows: g++ -o Guia0301.exe ./Guia0301.cpp
+ * 
+ * Para executar em um terminal:
+ * 
+ * No Linux  : ./Guia0301
+ * No Windows:   Guia0301
+ * 
+ */
+
+// lista de dependencias
+#include "karel.hpp"
+#include "io.hpp"
+
+// --------------------------- definicoes de metodos
+/**
+ * decorateWorld - Metodo para preparar o cenario
+ * @param fileName - nome do arquivo para guardar a descricao
+ */
+void decorateWorld(const char* fileName)
+{
+    // colocar paredes no mundo
+    world->set(4, 4, VWALL);
+    world->set(4, 4, HWALL);
+
+    // colocar um marcador no mundo
+    world->set(4, 4, BEEPER);
+
+    // salvar a configuracao atual do mundo
+    world->save(fileName);
+}
+
+/**
+ * Classe para definir robo particular (MyRobot),
+ * a partir do modelo generico (Robot)
+ *
+ * Nota: todas as definicoes irao valer para qualquer outro robo
+ * criado a partir dessa nova descricao de modelo.
+ */
+class MyRobot : public Robot
+{
+public:
+    /*
+      turnRight - Procedimento para virar 'a direita
+    */
+   void turnRight()
+   {
+        //definir dado local
+        int step = 0;
+
+        //testar se o robo esta' ativo
+        if (checkStatus())
+        {
+            // o agente que executar esse metodo
+            // devera' virar tres vezes 'a esquerda
+            for (step; step < 3; step++)
+            {
+                turnLeft();
+            }
+        }
+    }
+
+    /**
+     * moveN - Metodo para mover certa quantidade de passos
+     * @param steps - passos a serem dados
+     */
+    void moveN(int steps)
+    {   
+        // testar se a quantidade de passos e' maior que zero
+        for (int step = steps; step > 0; step--)
+        {
+            // dar um passo
+            move();
+        }
+    }
+
+    /**
+     * countCommands - Metodo para contar comandos do arquivo
+     * @param fileName - nome do arquivo
+     */
+    void countCommands(const char *fileName)
+    {
+        // definir dados
+        char message[80];
+        int x = 0;
+        int length = 0;
+        
+        // abrir arquivo para leitura
+        std::ifstream archive(fileName);
+        
+        // repetir enquanto houver dados
+        archive >> x;
+        while (! archive.eof())
+        {
+            // contar mais um comando
+            length++;
+            // tentar ler a proxima linha
+            archive >> x;
+        }
+        // fechar o arquivo
+        archive.close();
+
+        // informar a quantidade de comandos guardados
+        sprintf(message, "Commands = %d", length);
+        has_Text = true;
+        show_Text(message);
+    }
+};
+
+// --------------------------- acao principal
+/*
+  Acao principal: executar a tarefa descrita acima
+*/
+
+int main()
+{
+    // definir o contexto
+
+    // criar o ambiente e decorar com objetos
+    // OBS.: executar pelo menos uma vez,
+    //       antes de qualquer outra coisa
+    //       (depois de criado, podera' ser comentado)
+    world->create("");            // criar o mundo
+    decorateWorld("Guia0301.txt");
+    world->show();
+
+    // preparar o ambiente para uso
+    world->reset();               // limpar configuracoes
+    world->read("Guia0301.txt");  // ler configuracao atual para o ambiente
+    world->show();                // mostrar a configuracao atual
+
+    set_Speed(3);                 // definir velocidade padrao
+
+    // criar robo
+    MyRobot *robot = new MyRobot();
+
+    // posicionar robo no ambiente (situacao inicial):
+    // posicao (x=1, y=1), voltado para direita,
+    // com zero marcadores, nome escolhido)
+    robot->create(1, 1, NORTH, 0, "Karel");
+
+    // executar tarefa
+    robot->countCommands("Tarefa0301.txt");
+
+    // encerrar operacoes no ambiente
+    world->close();
+
+    // encerrar programa
+    getchar();
+    return(0);
+}
+
+// --------------------------------- testes
+/*
+------------------------------------ documentacao complementar
+------------------------------------ notas / observacoes / comentarios
+------------------------------------ historico
+Versao    Data    Modificacao
+ 0.1     19/3/21    esboco
+------------------------------------ testes
+Versao    Teste
+ 0.1     01. (OK)    teste inicial
+*/
